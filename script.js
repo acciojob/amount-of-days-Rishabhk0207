77a1c39085106fa54acb1a54be9db81458ecc1e1
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-04-24 19:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return 366; 
+  } else {
+    return 365; 
+  }
+}
+
+console.log(2022);
+console.log(2024);
+console.log(1900);
+console.log(2000);
